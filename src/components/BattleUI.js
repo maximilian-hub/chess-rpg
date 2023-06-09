6e6abca6f5452799628bb23f45324b7282c3dfbf
maximilian-hub <maximilian.spedale@gmail.com>
2023-06-09 12:52:50 -0500

    Added placeholder BattleUI.

--- src/components/BattleUI.js
+++ src/components/BattleUI.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import { useDispatch } from "react-redux";
+import { setScene } from "../store/slices/gameSlice";
 
-export default function BattleUI() {
-  return <div id="battle-ui">BattleUI</div>;
+export default function BattleUI(props) {
+  const dispatch = useDispatch();
+
+  function handleBackButton() {
+    dispatch(setScene("TitleScene"));
+  }
+
+  return (
+    <div id="title-ui">
+      Battle Scene
+      <div id="menu-item-container">
+        <div id="menu-item" onClick={handleBackButton}>
+          Back to Title
+        </div>
+      </div>
+    </div>
+  );
 }
